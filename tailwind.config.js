6a0b76ce43b20fda8b9a863d030a1d24199051a3
Amara Barton <codemelon.ab@gmail.com>
2025-06-25 18:12:06 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      antiqueWhite:       '#faebd7', 
+      alabaster:          '#eefef8', 
+
+      puce:               'cc8899',
+      cherryBlossomPink:  '#e9acbb', 
+      softPink:           '#f5c8c4', 
+      springCoral:        '#f59c9a', 
+      peachFuzz:          '#ffbe98', 
+
+      peach:              '#fcebbf', 
+      butterYellow:       '#ffe7ab', 
+
+      columbiaBlue:       '#c9b297', 
+      periwinklePaleBlue: '#9eccf0', 
+      azure:              '#d6e6e7', 
+
+      cambridgeBlue:      '#8fbc93', 
+      pistachioGreen:     '#c5dba9', 
+      citron:             '#c5cc82', 
+      mossGreen:          '#97a13b', 
+
+      amaranthPink:       '#e0a3bb', 
+      lavenderBlush:      '#f4dfe6',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
