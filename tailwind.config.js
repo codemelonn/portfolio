2903e3118395b3f0674d6684c15a7fffa9d94256
Amara Barton <codemelon.ab@gmail.com>
2025-06-26 00:09:11 -0600

    issue 1 completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
-  theme: {
-    colors: {
-      antiqueWhite:       '#faebd7', 
-      alabaster:          '#eefef8', 
+    content: ["index.html"],
+    theme: {
+        colors: {
+            antiqueWhite: "#faebd7",
+            alabaster: "#eefef8",
 
-      puce:               'cc8899',
-      cherryBlossomPink:  '#e9acbb', 
-      softPink:           '#f5c8c4', 
-      springCoral:        '#f59c9a', 
-      peachFuzz:          '#ffbe98', 
+            puce: "#cc8899",
+            cherryBlossomPink: "#e9acbb",
+            softPink: "#f5c8c4",
+            springCoral: "#f59c9a",
+            peachFuzz: "#ffbe98",
 
-      peach:              '#fcebbf', 
-      butterYellow:       '#ffe7ab', 
+            peach: "#fcebbf",
+            butterYellow: "#ffe7ab",
 
-      columbiaBlue:       '#c9b297', 
-      periwinklePaleBlue: '#9eccf0', 
-      azure:              '#d6e6e7', 
+            columbiaBlue: "#c9b297",
+            periwinklePaleBlue: "#9eccf0",
+            azure: "#d6e6e7",
 
-      cambridgeBlue:      '#8fbc93', 
-      pistachioGreen:     '#c5dba9', 
-      citron:             '#c5cc82', 
-      mossGreen:          '#97a13b', 
+            cambridgeBlue: "#8fbc93",
+            pistachioGreen: "#c5dba9",
+            citron: "#c5cc82",
+            mossGreen: "#97a13b",
 
-      amaranthPink:       '#e0a3bb', 
-      lavenderBlush:      '#f4dfe6',
+            amaranthPink: "#e0a3bb",
+            lavenderBlush: "#f4dfe6",
+        },
+
+        fontFamily: {
+            byteBounce: ['"Byte Bounce"', "sans-serif"],
+        },
+
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
