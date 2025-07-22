86589e45511238639a5027b3b389cf18149083c1
Amara Barton <codemelon.ab@gmail.com>
2025-07-22 17:08:27 -0600

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,83 +3,43 @@ module.exports = {
     content: ["./src/**/*.html", "./src/**/*.js"],
     theme: {
         colors: {
-            antiqueWhite: "#faebd7",
-            alabaster: "#eefef8",
+            // BROWNS / NEUTRALS
+            vanilla: "#fff9f5",
+            vanillaCream: "#fff4ec",
+            sailboatMarina: "#f2ead5",
+            bone: "#e6d7c4",
+            tan: "#cfbb9a",
+            milkTeaBrown: "#a77966",
+            milkBrown: "#a2756c",
+            ashyBrown: "#5e473d",
+            coffeeBrown: "#5e4034",
+            cocoaBrown: "#6e4b47",
+            cocoaText: "#4a2e2a",
+            ink: "#38221a",
+            inkBlack: "#2f1e1e",
+            cafeNoir: "#4b3d1a",
 
-            puce: "#cc8899",
-            cherryBlossomPink: "#e9acbb",
-            softPink: "#f5c8c4",
-            springCoral: "#f59c9a",
-            peachFuzz: "#ffbe98",
+            // GREENS / EARTH TONES
+            mintGlint: "#d0eed6",
+            mintDust: "#cbe3c0",
+            mossGreen: "#899064",
+            melonRind: "#a7a155",
+            kombuGreen: "#364025",
 
-            peach: "#fcebbf",
-            butterYellow: "#ffe7ab",
-
-            columbiaBlue: "#c9b297",
-            periwinklePaleBlue: "#9eccf0",
-            azure: "#d6e6e7",
-
-            cambridgeBlue: "#8fbc93",
-            pistachioGreen: "#c5dba9",
-            citron: "#c5cc82",
-            mossGreen: "#97a13b",
-
-            amaranthPink: "#e0a3bb",
-            lavenderBlush: "#f4dfe6",
-
-            sweetPink: "#f1d1da",
-            windowGrey: "#bebbb0",
-            titlePink: "#eeb1bf",
-
-            headerGrey: "#9c9a97",
-            headerOrange: "#eea971",
-            headerBlue: "#4f8fab",
-            mauve: "#b9868d",
-            bodyTan: "#c7afa8",
-            bodyTan2: "#c8b1a8",
-            bodyTan3: "#a99890",
-            iceBlue: "#cdd7dd",
-            iceBlueDark: "#748191",
-
-            retroShellPink: "#F4D1D2",
-            dustyBlush: "#E8B4BC",
-            velvetMauve: "#B97C86",
-            smokyQuartz: "#6B5B63",
-            palePebble: "#F8F6F3",
-            steelRose: "#9F8B94",
-            powderLilac: "#D4C4DC",
-            digitalGrape: "#7B6F96",
-            apricotCream: "#FBE8DE",
-
-            blushMilk: "#F6D3D6",
-            petalMauve: "#D7A3AB",
-            almondCream: "#FDF5F3",
-            fogLilac: "#EDE6F2",
-            petalFog: "#F4E8F2",
-
-            lightBrown: "#f0e6dc",
-
+            // YELLOWS / WARM TONES
             sweetTarts: "#d3c98b",
             nuttyBrown: "#a66c44",
+
+            // PINKS / BLUSHES
+            whisperPink: "#fce9ec",
+            blushPink: "#f2b6cc", 
+            bubblegumPink: "#fca3bf",
+            shellPink: "#f7b2b7",
             dutchTulips: "#f5b6a7",
-            melonRind: "#a7a155",
-            sailboatMarina: "#f2ead5",
-            cafeNoir: "#4b3d1a",
-            kombuGreen: "#364025",
-            mossGreen: "#899064",
-            tan: "#cfbb9a",
-            bone: "#e6d7c4",
             roseBlush: "#d8a48f",
             peachBlush: "#bb8487",
-
-            blushPink: "#f2b6c6",
-            bubblegumPink: "#fca3b7",
-            coffeeBrown: "#5e4034",
-            milkTeaBrown: "#a77966",
-            vanillaCream: "#fff4ec",
-            mintDust: "#cbe3c0",
-            cocoaText: "#4a2e2a",
-            inkBlack: "#2f1e1e",
+            sweetPink: "#ffd3da",
+            sweeterPink: "#ffdbe0", 
         },
 
         fontFamily: {
