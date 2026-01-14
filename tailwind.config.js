962bbbb9eceae8381e86567d138542edae41f8ed
Amara Barton <codemelon.ab@gmail.com>
2026-01-14 10:01:15 -0700

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,61 +2,56 @@
 module.exports = {
     content: ["./src/**/*.html", "./src/**/*.js"],
     theme: {
-        colors: {
-            // BROWNS / NEUTRALS
-            vanilla: "#fff9f5",
-            vanillaCream: "#fff3ec",
-            sailboatMarina: "#f2ead5",
-            bone: "#e6d7c4",
-            tan: "#cfbb9a",
-            milkTeaBrown: "#a58d7f",
-            milkBrown: "#a2756c",
-            ashyBrown: "#5e473d",
-            coffeeBrown: "#5e4034",
-            cocoaBrown: "#6e4b47",
-            cocoaText: "#5c4238",
-            ink: "#38221a",
-            inkBlack: "#2f1e1e",
-            cafeNoir: "#4b3d1a",
-            blushMilk: '#fae7e7',
+        extend: {
+            colors: {
+                // BROWNS / NEUTRALS
+                vanilla: "#fff9f5",
+                vanillaCream: "#fff3ec",
+                sailboatMarina: "#f2ead5",
+                bone: "#e6d7c4",
+                tan: "#cfbb9a",
+                milkTeaBrown: "#a58d7f",
+                milkBrown: "#a2756c",
+                ashyBrown: "#5e473d",
+                coffeeBrown: "#5e4034",
+                cocoaBrown: "#6e4b47",
+                cocoaText: "#5c4238",
+                ink: "#38221a",
+                inkBlack: "#2f1e1e",
+                cafeNoir: "#4b3d1a",
+                blushMilk: "#fae7e7",
 
-            // GREENS / EARTH TONES
-            mintGlint: "#d0eed6",
-            mintDust: "#cbe3c0",
-            mossGreen: "#899064",
-            melonRind: "#a7a155",
-            kombuGreen: "#364025",
+                // GREENS / EARTH TONES
+                mintGlint: "#d0eed6",
+                mintDust: "#cbe3c0",
+                mossGreen: "#899064",
+                melonRind: "#a7a155",
+                kombuGreen: "#364025",
 
-            // YELLOWS / WARM TONES
-            sweetTarts: "#d3c98b",
-            nuttyBrown: "#a66c44",
+                // YELLOWS / WARM TONES
+                sweetTarts: "#d3c98b",
+                nuttyBrown: "#a66c44",
 
-            // PINKS / BLUSHES
-            whisperPink: "#fce9ec",
-            blushPink: "#f2b6cc", 
-            bubblegumPink: "#fca3bf",
-            shellPink: "#f7b2b7",
-            dutchTulips: "#f5b6a7",
-            roseBlush: "#d8a48f",
-            peachBlush: "#bb8487",
-            sweetPink: "#ffd3da",
-            sweeterPink: "#ffdbe0", 
+                // PINKS / BLUSHES
+                whisperPink: "#fce9ec",
+                blushPink: "#f2b6cc",
+                bubblegumPink: "#fca3bf",
+                shellPink: "#f7b2b7",
+                dutchTulips: "#f5b6a7",
+                roseBlush: "#d8a48f",
+                peachBlush: "#bb8487",
+                sweetPink: "#ffd3da",
+                sweeterPink: "#ffdbe0",
+            },
+
+            fontFamily: {
+                vpPixel: ['"VP Pixel"', "sans-serif"],
+                blockBlueprint: ['"Block Blueprint"', "sans-serif"],
+                MicrosoftOG: ['"Microsoft OG"', "sans-serif"],
+                VCR: ['"VCR"', "sans-serif"],
+                NDS: ['"NDS"', "sans-serif"],
+            },
         },
-
-        fontFamily: {
-            byteBounce: ['"Byte Bounce"', "sans-serif"],
-            orangeKid: ['"Orange Kid"', "sans-serif"],
-            retroByte: ['"Retro Byte"', "sans-serif"],
-            vpPixel: ['"VP Pixel"', "sans-serif"],
-            computerPixel: ['"Computer Pixel"', "sans-serif"],
-            ffPath: ['"FF Path"', "sans-serif"],
-            blockBlueprint: ['"Block Blueprint"', "sans-serif"],
-            MicrosoftOG: ['"Microsoft OG"', "sans-serif"],
-            VCR: ['"VCR"', "sans-serif"],
-            AdobeDia: ['"Adobe Dia"', "sans-serif"], 
-        },
-
-        extend: {},
     },
     plugins: [],
 };
